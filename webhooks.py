dac10b63544b39b308317b0ad75061a8addd208d
Seongbin Hong <37045096+VertexToEdge@users.noreply.github.com>
2023-11-12 19:53:05 +0900

    Update webhooks.py

--- webhooks.py
+++ webhooks.py
@@ -21,4 +21,4 @@ class Webhook(Resource):
 api.add_resource(Webhook, '/deploy')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
